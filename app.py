3677fcaca4a1089b694aa075c43330ae16746634
trachman <tsrachman7@gmail.com>
2021-01-27 19:33:04 -0500

    finds current price of TSLA

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-print('hi')
+def get_current_price(COMPANY): 
+	yfurl = 'https://ca.finance.yahoo.com/quote/{}?p={}&.tsrc=fin-srch'.format(COMPANY,COMPANY) 
+	response = requests.get(yfurl)
+	soup = BeautifulSoup(response.content, 'html.parser')
+	data = soup.find('div', {'class' : 'D(ib) Mend(20px)'})
+	# format is [current trading price, dollar/pct change, last time updated]	
+	return [item.text for item in data] 
+
+def main():
+	form = get_current_price('TSLA')
+	print(form)
+
+if __name__ == '__main__':
+	main()
